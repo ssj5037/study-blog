72e79e11fb92070174029b1e99b7e956aedfe398
sujin shin <shinsj5037@gmail.com>
2023-09-11 17:54:56 +0900

    home 추가

--- src/component/Home.tsx
+++ src/component/Home.tsx
@@ -1,5 +1,52 @@
+import { Box, Grid, GridItem, HStack, Heading, Image, Link, Text, VStack } from "@chakra-ui/react"
+
 export const Home = () => {
     return (
-        <h1> Home</h1>
+        <>
+            <Grid
+                id="HomeGrid"
+                h='900px'
+                templateRows='repeat(2, 1fr)'
+                templateColumns='repeat(10, 1fr)'
+                gap={10}
+                m='20px 20px 0 20px'
+            >
+                <GridItem colSpan={3} >
+                    <img src="/image/home1.jpg" height='350px'></img>
+                </GridItem>
+                <GridItem colSpan={3} >
+                    <img src="/image/home2.jpg"></img>
+                </GridItem>
+                <GridItem colSpan={4} rowSpan={2} textAlign='left' >
+                    <Box mt='20'>
+                        <VStack>
+                            <Text fontSize='sm'>웹 풀스택 개발자</Text>
+                            <Heading size='3xl'>신 수 진</Heading>
+                        </VStack>
+                    </Box>
+                    <Box mt='20'>
+                        <Text fontSize='lg'>비즈앤플랫폼(2023.04 ~ 2023.06) - 프론트엔드 개발자</Text>
+                        <Text fontSize='sm'>중소기업형 렌터카 사업체를 위한 자동차 대여 시스템 프론트엔드 개발 업무 담당.</Text>
+                        <Text fontSize='sm'>일부 백엔드 업무도 수행하였음.</Text>
+                    </Box>
+                    <Box mt='8'>
+                        <Text fontSize='lg'>크로니즈시스템(2020.10 ~ 2023.03) - 풀스택 개발자</Text>
+                        <Text fontSize='sm'>MES 시스템, Smart Farm시스템 개발 및 설계 업무 담당.</Text>
+                    </Box>
+                    <HStack mt='8'>
+                        <Link href="https://github.com/ssj5037" isExternal >
+                            <Image src="https://github.githubassets.com/images/modules/site/icons/footer/github-mark.svg" />
+                        </Link>
+                    </HStack>
+                </GridItem>
+
+                <GridItem colSpan={3} >
+                    <img src="/image/home3.jpg"></img>
+                </GridItem>
+                <GridItem colSpan={3} >
+                    <img src="/image/home4.jpg"></img>
+                </GridItem>
+            </Grid>
+        </>
     )
 }
--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,12 +1,11 @@
 
 import { ChakraProvider, Avatar, Center, Grid, GridItem, Input, Button,
-  Menu, MenuButton, MenuItem, MenuList, MenuGroup, HStack
+  Menu, MenuButton, MenuItem, MenuList, MenuGroup, HStack, IconButton
 } from '@chakra-ui/react';
-import { ChevronDownIcon } from '@chakra-ui/icons';
+import { ChevronDownIcon, SearchIcon } from '@chakra-ui/icons';
 
 import { blogMenuList, BlogMenu } from '../data/menu';
 import { Outlet, Link  } from 'react-router-dom';
-
 
 export const Layout = () => {
     
@@ -52,7 +51,7 @@ export const Layout = () => {
 
 
     return (
-        <ChakraProvider>
+      <ChakraProvider>
         <Center>
           <Grid
           width={1540}
@@ -60,76 +59,49 @@ export const Layout = () => {
                           "main"
                           "footer"`}
           gridTemplateRows={'80px 10fr 60px'}
-          h='100vh'
           gap='1'
           color='blackAlpha.700'
           fontWeight='bold'
-        >
-          {/* =========== Header =========== */}
-          <GridItem pl='2' bg='white' area={'header'}>
-            <Grid templateColumns='200px 3fr 250px 100px' gap={6}>
-              <Center h='80px'>
-                <h1>🤍 Sujin&apos;s Study Blog</h1>
-              </Center>
+          >
+            {/* =========== Header =========== */}
+            <GridItem pl='2' bg='white' area={'header'} borderBottom='1px' borderBottomColor='blackAlpha.200'>
+              <Grid templateColumns='200px 3fr 250px 100px' gap={6}>
+                <Center h='80px'>
+                  <Link to="/">
+                    <h1>🤍 Sujin&apos;s Blog </h1>
+                  </Link>
+                </Center>
                 <HStack spacing='24px'>
                   { menuList }
-              </HStack>
-              <Center h='80px'>
-                <Input variant={'flushed'} size={'sm'}></Input>
-                <Button variant={'outline'} size={'sm'} ml={2}> 검색 </Button>
-              </Center>
-              <Center h='80px'>
-                <Avatar name='Dan Abrahmov' src='https://bit.ly/dan-abramov' />
-              </Center>
-            </Grid>
-          </GridItem>
-  
-          {/* =========== Navigation =========== */}
-          {/* <GridItem pl='2' area={'nav'}>
-            <Accordion defaultIndex={[0]} allowMultiple>
-              <AccordionItem>
-                <h2>
-                  <AccordionButton>
-                    <Box as="span" flex='1' textAlign='left'>
-                      대분류 1
-                    </Box>
-                    <AccordionIcon />
-                  </AccordionButton>
-                </h2>
-                <Divider />
-                <AccordionPanel pb={4}>
-                  소분류 1
-                </AccordionPanel>
-              </AccordionItem>
-  
-              <AccordionItem>
-                <h2>
-                  <AccordionButton>
-                    <Box as="span" flex='1' textAlign='left'>
-                      대분류 2
-                    </Box>
-                    <AccordionIcon />
-                  </AccordionButton>
-                </h2>
-                <Divider />
-                <AccordionPanel pb={4}>
-                  소분류 1
-                </AccordionPanel>
-              </AccordionItem>
-            </Accordion>
-          </GridItem> */}
-  
-          {/* =========== Main =========== */}
-          <GridItem pl='2' area={'main'}>
-                <Outlet />
-          </GridItem>
-  
-          {/* =========== Footer =========== */}
-          <GridItem pl='2' area={'footer'}>
-              Copyright © SuJin-Shin. All Rights Reserved.<br/>
+                </HStack>
+                <Center h='80px'>
+                  <Input variant={'flushed'} size={'sm'}></Input>
+                <IconButton
+                    variant='outline'
+                    size={'sm'} ml={2}
+                    aria-label='Call Sage'
+                    fontSize='20px'
+                    icon={<SearchIcon />}
+                />
+                  {/* <Button variant={'outline'} size={'sm'} ml={2}> 검색 </Button> */}
+                </Center>
+                <Center h='80px'>
+                  <Avatar name='Dan Abrahmov' src='/image/avatar1.jpg' />
+                </Center>
+              </Grid>
+            </GridItem>
+    
+            {/* =========== Main =========== */}
+            <GridItem pl='2' area={'main'}>
+              <Outlet />
+            </GridItem>
+    
+            {/* =========== Footer =========== */}
+            <GridItem pl='2' area={'footer'}>
+              Copyright © Su-Jin Shin. All Rights Reserved.<br/>
               {/* E-mail. shinsj5037@gmail.com<br/>
               github. https://github.com/ssj5037 */}
-          </GridItem>
+            </GridItem>
           </Grid>
         </Center>
       </ChakraProvider>
